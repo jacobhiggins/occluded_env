f658c17597a70ff2151f42a7683975f7f4cfc11b
jacobhiggins <jdh4je@virginia.edu>
2020-07-19 20:07:28 -0400

    Update runsim.m

--- sims/point_mass_sim9/runsim.m
+++ sims/point_mass_sim9/runsim.m
@@ -1,9 +1,7 @@
 % Run sim for point-mass
 addpath(genpath('C:\Users\bezzo\Google Drive\Robotics\thinkPad_ws\Sims\acado_tests\robotsim\mathworks-robotics-mobile-robotics-simulation-toolbox-7066fa0'));
-addpath('../point_mass_export4')
-addpath('../point_mass_export')
-addpath('../point_mass_export5')
-addpath('../point_mass_export6')
+addpath('../../acado_code/point_mass_export6')
+addpath('../../acado_code/point_mass_export7')
 addpath('./utils')
 
 close all;
